3742447e01eb994e37b578466b24b1c63437d70e
Surya Janardhan <chintalajanardhan2004@gmail.com>
2026-02-09 15:31:33 +0530

    change port as per reqs

--- api-service/src/main.js
+++ api-service/src/main.js
@@ -14,7 +14,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 // middleware
 app.use(express.json());
