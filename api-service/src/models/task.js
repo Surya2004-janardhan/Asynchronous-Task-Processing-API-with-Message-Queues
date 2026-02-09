e5f7a6223983bb23a352d1766f2dc0cae02a03bb
Surya Janardhan <surya@growstack.biz>
2026-02-09 14:19:42 +0530

    model : core funcs created to interact with sql tasks table

--- api-service/src/models/task.js
+++ api-service/src/models/task.js
@@ -7,3 +7,47 @@
  * 3. Provide methods for fetching tasks by ID (find).
  * 4. Map database rows to JavaScript objects.
  */
+
+// interaction logic with sql table tht we created in init.sql
+
+// const db = require('../config/database');
+// fixing correct path for db 
+const db = require('../db/init.sql');
+
+// create task
+async function create(task) {
+    const [result] = await db.query('INSERT INTO tasks SET ?', task);
+    return result.insertId;
+}
+
+// find task by id
+async function find(id) {
+    const [rows] = await db.query('SELECT * FROM tasks WHERE id = ?', id);
+    return rows[0];
+}
+
+// update task
+async function update(id, task) {
+    const [result] = await db.query('UPDATE tasks SET ? WHERE id = ?', [task, id]);
+    return result.affectedRows;
+}
+
+// delete task
+async function remove(id) {
+    const [result] = await db.query('DELETE FROM tasks WHERE id = ?', id);
+    return result.affectedRows;
+}
+
+// get all tasks
+async function findAll() {
+    const [rows] = await db.query('SELECT * FROM tasks');
+    return rows;
+}
+
+module.exports = {
+    create,
+    find,
+    update,
+    remove,
+    findAll
+};
