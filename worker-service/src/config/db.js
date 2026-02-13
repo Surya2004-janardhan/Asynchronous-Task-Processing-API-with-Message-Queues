ebe11b0873c2f44f856d0c4a6a442c000ca3bb19
Surya Janardhan <chintalajanardhan2004@gmail.com>
2026-02-13 10:06:14 +0530

    change env var name for db

--- worker-service/src/config/db.js
+++ worker-service/src/config/db.js
@@ -13,7 +13,7 @@ const pool = mysql.createPool({
     host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || 'password',
-    database: process.env.MYSQL_DB || 'task_db',
+    database: process.env.MYSQL_DATABASE || 'task_db',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
